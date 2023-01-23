d92047880a074f98c10c1efe4c68850960058274
YiFan Chen <yifanchen@YiFans-MacBook-Air.local>
2023-01-22 20:06:56 -0500

    same as accountExpressAPI changes

---
+++ server/api/entryRouters.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response, NextFunction } from "express";
+import { Entry } from "../db/index.js";
+import { EntryAttributes } from "../db/models/Entry.model.js";
+const router = express.Router();
+
+router.get("/", async (req: Request, res: Response, next: NextFunction) : Promise<void>=> {
+  try {
+    const entries: EntryAttributes[] = await Entry.findAll();
+    res.send(entries);
+  } catch (err) {
+    res.sendStatus(404);
+    next(err);
+  }
+});
+
+router.post("/", async (req: Request, res: Response, next: NextFunction): Promise<void>=> {
+    try {
+      const {entryType, date, creditDebit, amount, title, note, frequency} : EntryAttributes = req.body
+      // const createEntry = 
+      await Entry.create({
+        entryType, date, creditDebit, amount, title, note, frequency
+      })
+      res.sendStatus(204)
+    } catch (err) {
+      res.sendStatus(404);
+      next(err);
+    }
+  });
+  
+  router.delete("/:entryId", async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const entryId: string = req.params.entryId
+      //soimething weird happens since 
+      //accountId starts string but ends up number??
+      //so TS forces it to be string idk
+      const entryToDelete = await Entry.findByPk(entryId)
+      entryToDelete?.destroy()
+      res.sendStatus(204)
+    } catch (err) {
+      res.sendStatus(404);
+      next(err);
+    }
+  });
+  
+
+export default router;
