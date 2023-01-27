97b3e95d185c51600cf71384d39b14aa56170023
JackPadalino <pada0867@gmail.com>
2023-01-27 06:24:39 -0500

    updated API index to include goalsRouter

--- server/api/index.ts
+++ server/api/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import userRouter from "./userRouter.js";
 import accountRouter from "./accountRouter.js";
 import authRouter from "./auth.js";
+import goalsRouter from "./goalsRouter.js";
 const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
 router.use("/account", accountRouter);
+router.use("/goals", goalsRouter);
 
 export default router;
