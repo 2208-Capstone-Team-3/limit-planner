563e19eeab13e217c03ba4cec81d3157febe5e94
AOKaizoMac <kolbywolf@gmail.com>
2023-01-30 18:21:44 -0500

    Basic Front Page Data Ready

--- src/Components/Home/HomeBasePortal.tsx
+++ src/Components/Home/HomeBasePortal.tsx
@@ -1,8 +1,25 @@
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import React from "react";
+import MainGoalPercentageChart from "../Charts/MainGoalPercentageChart";
 import MainLineChart from "../Charts/MainLineChart";
 
 const HomeBasePortal = () => {
-  return <MainLineChart />;
+  return (
+    <Grid2 container height={"90vh"} width={"auto"}>
+        <Grid2 xs={6}>
+            <div>Calendar Placeholder</div>
+        </Grid2>
+      <Grid2 xs={6}>
+        <MainLineChart />
+      </Grid2>
+      <Grid2 xs={6}>
+       <div>Selector Placeholder</div>
+      </Grid2>
+      <Grid2 xs={6}>
+        <MainGoalPercentageChart />
+      </Grid2>
+    </Grid2>
+  );
 };
 
 export default HomeBasePortal;
