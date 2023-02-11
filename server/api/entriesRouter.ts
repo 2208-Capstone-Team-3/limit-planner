79837957ea900c6e3992fe77208e62a70fb8d0cc
JackPadalino <pada0867@gmail.com>
2023-02-11 09:40:15 -0500

    refactored API routes

--- server/api/entriesRouter.ts
+++ server/api/entriesRouter.ts
@@ -2,122 +2,142 @@ import express, { Request, Response, NextFunction } from "express";
 import { Account, Entry, User } from "../db/index.js";
 import { AccountAttributes } from "../db/models/Account.model.js";
 import { EntryAttributes } from "../db/models/Entry.model.js";
+import { UserAttributes } from "../db/models/User.model.js";
 import { authenticateUser } from "./helpers/authUserMiddleware.js";
 const router = express.Router();
 
-router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+router.get(
+  "/:entryId", 
+  authenticateUser,
+  async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const header = req.headers.authorization;
-    const token = header && header.split(" ")[1];
-
-    if (!token) return res.sendStatus(404);
-
-    const foundUser = await User.prototype.findByToken(token);
-
-    if (!foundUser.id) {
-      return res.sendStatus(404);
-    }
-
-    const foundUserInfo: AccountAttributes[] | null = await Account.findAll({
-      where: { userId: foundUser.id },
-      include: [Entry],
-    });
-
-    if (foundUserInfo) {
-      const entryInfoOnly = foundUserInfo.map((acc) => acc.entries);
-      res.status(200).send(entryInfoOnly);
-    }
+    const foundEntry:EntryAttributes | null = await Entry.findByPk(req.params.entryId);
+    if(foundEntry) res.send(foundEntry);
+    else res.sendStatus(404);
   } catch (err) {
     res.sendStatus(404);
     next(err);
-  }
+  };
+});
+
+router.get(
+  "/", 
+  authenticateUser,
+  async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const foundUser:UserAttributes | null = await User.findByPk(req.body.user.id);
+    if(!foundUser){
+      res.sendStatus(404);
+    }else{
+      const foundUserAccounts: AccountAttributes[] | null = await Account.findAll({
+        where: { userId: foundUser?.id },
+        include: [Entry]
+      });
+      if(!foundUserAccounts){
+        res.sendStatus(404);
+      }else{
+        const userEntries = foundUserAccounts.map((acc) => acc.entries);
+        res.send(userEntries);
+      };
+    };
+  } catch (err) {
+    res.sendStatus(404);
+    next(err);
+  };
 });
 
 router.post(
   "/",
   authenticateUser,
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // const user = req.body.user;
-    const account = req.body.account;
     try {
-      const {
-        entryType,
-        start,
-        creditDebit,
-        amount,
-        title,
-        note,
-        allDay,
-        frequency,
-      }: EntryAttributes = req.body;
-      const createdEntry = await Entry.create({
-        entryType,
-        start,
-        creditDebit,
-        amount,
-        title,
-        note,
-        allDay,
-        frequency
-      });
-      account.addEntry(createdEntry);
-      res.sendStatus(204);
+      const account:AccountAttributes | null = await Account.findByPk(req.body.accountId);
+      if(!account){
+        res.sendStatus(404);
+      }else{
+        const {
+          entryType,
+          start,
+          creditDebit,
+          amount,
+          title,
+          note,
+          allDay,
+          frequency,
+        }: EntryAttributes = req.body;
+        const createdEntry = await Entry.create({
+          entryType,
+          start,
+          creditDebit,
+          amount,
+          title,
+          note,
+          allDay,
+          frequency
+        });
+        account.addEntry(createdEntry);
+        res.sendStatus(204);
+      };
     } catch (err) {
       res.sendStatus(404);
       next(err);
-    }
-  })
+    };
+});
 
-// router.post(
-//   "/",
-//   authenticateUser,
-//   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-//     // const user = req.body.user;
-//     const account = req.body.account;
-//     try {
-//       const {
-//         entryType,
-//         date,
-//         creditDebit,
-//         amount,
-//         title,
-//         note,
-//         frequency,
-//       }: EntryAttributes = req.body;
-//       const createdEntry = await Entry.create({
-//         entryType,
-//         date,
-//         creditDebit,
-//         amount,
-//         title,
-//         note,
-//         frequency,
-//       });
-//       account.addEntry(createdEntry);
-//       res.sendStatus(204);
-//     } catch (err) {
-//       res.sendStatus(404);
-//       next(err);
-//     }
-//   }
-// );
+router.put(
+  "/:entryId",
+  authenticateUser,
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const foundEntry:EntryAttributes | null = await Entry.findByPk(req.params.entryId);
+      if(!foundEntry){
+        res.sendStatus(404);
+      }else{
+        const {
+          entryType,
+          start,
+          creditDebit,
+          amount,
+          title,
+          note,
+          allDay,
+          frequency,
+        }: EntryAttributes = req.body;
+        foundEntry.update({
+          entryType,
+          start,
+          creditDebit,
+          amount,
+          title,
+          note,
+          allDay,
+          frequency,
+        });
+        res.sendStatus(204);
+      }
+    } catch (err) {
+      res.sendStatus(404);
+      next(err);
+    };
+});
 
 router.delete(
   "/:entryId",
   authenticateUser,
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const entryId: string = req.params.entryId;
-      //soimething weird happens since
-      //entryId starts string but ends up number??
-      //so TS forces it to be string idk
-      const entryToDelete = await Entry.findByPk(entryId);
-      entryToDelete?.destroy();
-      res.sendStatus(204);
+      const entryToDelete:EntryAttributes | null = await Entry.findByPk(req.params.entryId);
+      if(!entryToDelete){
+        res.sendStatus(404);
+      }else{
+        entryToDelete?.destroy();
+        res.sendStatus(204);
+      };
     } catch (err) {
       res.sendStatus(404);
       next(err);
-    }
+    };
   }
 );
+
 export default router;
