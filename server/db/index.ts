1a404dfd28ecccf1834bd156e0bcd98bf02c46f7
JackPadalino <pada0867@gmail.com>
2023-02-12 11:42:15 -0500

    removed reference to Event model

--- server/db/index.ts
+++ server/db/index.ts
@@ -3,7 +3,6 @@ import User from "./models/User.model.js";
 import Account from "./models/Account.model.js";
 import Entry from "./models/Entry.model.js";
 import Goal from "./models/Goal.model.js";
-import Event from "./models/Event.model.js";
 import Skipdate from "./models/Skipdate.model.js"
 
 User.hasMany(Account);
@@ -30,4 +29,4 @@ Skipdate.belongsTo(Account)
 User.hasMany(Skipdate)
 Skipdate.belongsTo(User)
 
-export { Account, User, Goal, Entry, Event, Skipdate };
+export { Account, User, Goal, Entry, Skipdate };
