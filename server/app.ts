b1889c2d2a10d8be2d62874a78a307f68df15ec2
AOKaizoMac <kolbywolf@gmail.com>
2023-02-18 15:30:51 -0500

    I fixed it?

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import path from "path";
 import api from "./api/index.js";
+import { fileURLToPath } from "url";
 const app = express();
+const __dirname = fileURLToPath(import.meta.url);
 
 // Set up middleware
 app.use(express.json());
@@ -10,10 +12,20 @@ app.use(express.json());
 app.use("/api", api);
 
 // Pass back everything else / front-end
-app.use("/build", express.static(path.join(import.meta.url, "../build")));
-app.use("/public", express.static(path.join(import.meta.url, "../public")));
+app.use("/public", express.static(path.join(__dirname, "../public")));
+let root = path.join(__dirname, "..", "..", "build");
+console.log(root);
+app.use(express.static(root));
 
-app.get("*", function (req, res) {
-  res.sendFile("index.html", { root:  "./build" });
+app.use(function (req, res, next) {
+  if (
+    req.method === "GET" &&
+    req.accepts("html") &&
+    !req.is("json") &&
+    !req.path.includes(".")
+  ) {
+    res.sendFile("index.html", { root });
+  } else next();
 });
+
 export default app;
