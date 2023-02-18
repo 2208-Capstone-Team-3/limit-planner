b77eefefb2979bddc6dc0407496bb9bc078c9a21
AOKaizoMac <kolbywolf@gmail.com>
2023-02-17 20:37:56 -0500

    Server Work change

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,9 @@
 import express from "express";
 import path from "path";
-import morgan from "morgan";
 import api from "./api/index.js";
 const app = express();
 
 // Set up middleware
-app.use(morgan("combined"));
 app.use(express.json());
 
 // Set up routes
@@ -13,5 +11,10 @@ app.use("/api", api);
 
 // Pass back everything else / front-end
 app.use("/public", express.static(path.join(import.meta.url, "../public")));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 export default app;
