e0653766994ca1c89d11e1d28182383ee45b7b64
JackPadalino <pada0867@gmail.com>
2023-02-20 21:47:17 -0500

    removed goal chart and select goal functionality from component

--- src/Components/Charts/AdvancedCharting.tsx
+++ src/Components/Charts/AdvancedCharting.tsx
@@ -15,9 +15,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import {
   setAccountSelector,
-  setGoalSelector,
-  setFilteredEntries,
-  setFilteredGoals,
+  setFilteredEntries
 } from "../../store/themeSlice";
 import MainGoalPercentageChart from "./MainGoalPercentageChart";
 import MainHistogram from "./MainHistogram";
@@ -27,19 +25,11 @@ import MainScatterChart from "./MainScatterChart";
 const AdvancedCharting = () => {
   const dispatch = useDispatch();
   let accounts = useSelector((state: RootState) => state.accounts.accounts);
-  let goals = useSelector((state: RootState) => state.goals.goals);
-  const allGoals = goals.flat(Infinity).map((ele) => ele);
   const reoccurEntries = useSelector(
     (state: RootState) => state.reoccurEntries.reoccurEntries
   );
   let accountSelector = useSelector(
     (state: RootState) => state.theme.theme.accountSelector
-  );
-  let goalSelector = useSelector(
-    (state: RootState) => state.theme.theme.goalSelector
-  );
-  let filteredGoals = useSelector(
-    (state: RootState) => state.theme.theme.filteredGoals
   );
   const handleAccount = (ele: SelectChangeEvent) => {
     dispatch(setAccountSelector(JSON.parse(ele.target.value)));
@@ -47,13 +37,6 @@ const AdvancedCharting = () => {
       (entry) => entry.accountId === JSON.parse(ele.target.value).id
     );
     dispatch(setFilteredEntries(filteredEntries));
-    const filteredGoals = allGoals.filter(
-      (goal) => goal.accountId === JSON.parse(ele.target.value).id
-    );
-    dispatch(setFilteredGoals(filteredGoals));
-  };
-  const handleGoal = (ele: SelectChangeEvent) => {
-    dispatch(setGoalSelector(JSON.parse(ele.target.value)));
   };
   const [chartSelected, setChartSelected] = useState<string | null>(null);
   const handleChartSelect = (e: SelectChangeEvent) => {
@@ -79,7 +62,6 @@ const AdvancedCharting = () => {
               onChange={handleChartSelect}
             >
               <MenuItem value={"Line Chart"}>Line Chart</MenuItem>
-              <MenuItem value={"Goal Chart"}>Goal Chart</MenuItem>
               <MenuItem value={"Histogram"}>Histogram</MenuItem>
               <MenuItem value={"Scatter Chart"}>Scatter Chart</MenuItem>
             </Select>
@@ -119,41 +101,6 @@ const AdvancedCharting = () => {
               ))}
             </Select>
           </FormControl>
-        </Grid2>
-        <Grid2 xs={12}>
-        <FormControl fullWidth>
-          <InputLabel id="goalSelectLabel">Goal</InputLabel>
-          <Select
-            key={"goalSelect"}
-            fullWidth
-            labelId="goalSelectLabel"
-            id="goalSelect"
-            value={goalSelector ? JSON.stringify(goalSelector) : ""}
-            renderValue={(
-              ele:
-                | {
-                    id?: string | undefined;
-                    name: string;
-                    goalAmount: number;
-                    endDate: string | Date;
-                    victory: boolean;
-                  }
-                | string
-            ) => (
-              <Typography>
-                {typeof ele === "string" ? JSON.parse(ele).name : ele.name}
-              </Typography>
-            )}
-            label="Goal"
-            onChange={handleGoal}
-          >
-            {filteredGoals.map((ele, id) => (
-              <MenuItem key={`${ele.id}` + id} value={JSON.stringify(ele)}>
-                {ele.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
         </Grid2>
       </Grid2>
       <Divider orientation="vertical" flexItem sx={{ mr: "-1px" }} />
